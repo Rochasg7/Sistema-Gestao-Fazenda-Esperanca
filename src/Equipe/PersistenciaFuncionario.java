559340874897123be3b87991759fb37f3c6fc311
lugzh <lm817263@gmail.com>
2026-05-14 19:39:06 -0300

    percistencia funcionarios

--- src/Equipe/PersistenciaFuncionario.java
+++ src/Equipe/PersistenciaFuncionario.java
@@ -1,6 +1,8 @@
 package Equipe;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -12,16 +14,15 @@ public class PersistenciaFuncionario {
 
         try {
 
-            BufferedWriter writer = new BufferedWriter(
-                    new FileWriter("Dados/funcionarios.csv")
-            );
-
-            writer.write("Nome;Matricula;Contrato");
-            writer.newLine();
+            BufferedWriter writer =
+                    new BufferedWriter(
+                            new FileWriter("funcionarios.csv")
+                    );
 
             for (int i = 0; i < Registros.totalFuncionarios; i++) {
 
-                Funcionario funcionario = Registros.funcionarios[i];
+                Funcionario funcionario =
+                        Registros.funcionarios[i];
 
                 writer.write(
                         funcionario.nome + ";" +
@@ -34,11 +35,49 @@ public class PersistenciaFuncionario {
 
             writer.close();
 
-            System.out.println("Funcionários salvos com sucesso.");
+            System.out.println("Funcionários salvos com sucesso!");
 
         } catch (IOException e) {
 
-            System.out.println("Erro ao salvar arquivo.");
+            System.out.println("Erro ao salvar funcionários.");
+        }
+    }
+
+    public static void carregarFuncionariosCSV() {
+
+        try {
+
+            BufferedReader reader =
+                    new BufferedReader(
+                            new FileReader("funcionarios.csv")
+                    );
+
+            String linha;
+
+            while ((linha = reader.readLine()) != null) {
+
+                String[] partes = linha.split(";");
+
+                Funcionario funcionario = new Funcionario();
+
+                funcionario.nome = partes[0];
+                funcionario.matricula = partes[1];
+                funcionario.tipoContrato = partes[2];
+
+                Registros.funcionarios[
+                        Registros.totalFuncionarios
+                ] = funcionario;
+
+                Registros.totalFuncionarios++;
+            }
+
+            reader.close();
+
+            System.out.println("Funcionários carregados com sucesso!");
+
+        } catch (IOException e) {
+
+            System.out.println("Arquivo de funcionários não encontrado.");
         }
     }
 }
